58daf1aa134d66d59483e5f2331f4a68a6e8df38
Witold Kaczurba <witold.kaczurba@gmail.com>
2018-09-22 13:32:03 +0100

    Section1-24: String Interpolation

--- my-project/src/app/server/server.component.ts
+++ my-project/src/app/server/server.component.ts
@@ -5,5 +5,11 @@ import { Component } from '@angular/core';
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverId = 23
+    serverStatus = 'Offline'
+    
+    getAdmin() {
+        return `John O''Dowd`;
+    }
 }
 
